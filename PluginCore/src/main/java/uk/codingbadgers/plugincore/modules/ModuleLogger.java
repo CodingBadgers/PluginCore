af5506fad155322510f93d1f308e97f3fd39cbdb
James Fitzpatrick <thefishlive@hotmail.com>
2019-07-21 10:57:22 +0100

    Update module logger to support the module debug flag
    When debugging is disabled only log messages INFO and up are displayed.
    When debugging is enabled all log messages are shown.

--- PluginCore/src/main/java/uk/codingbadgers/plugincore/modules/ModuleLogger.java
+++ PluginCore/src/main/java/uk/codingbadgers/plugincore/modules/ModuleLogger.java
@@ -8,18 +8,31 @@ import java.util.logging.Logger;
 
 public class ModuleLogger extends Logger {
 
+    private static final int c_debugValue = Level.ALL.intValue();
+    private static final int c_normalValue = Level.INFO.intValue();
+
+    private Module m_module;
     private String m_prefix;
 
     protected ModuleLogger(PluginCore plugin, Module module) {
         super(module.getDescription().getName(), null);
 
+        m_module = module;
         m_prefix = "[" + plugin.getDescription().getName() + "] [" + module.getDescription().getName() + "] ";
+
         setParent(plugin.getLogger());
-        setLevel(Level.ALL);
+    }
+
+    @Override
+    public boolean isLoggable(Level level) {
+        return level.intValue() >= (m_module.isDebug() ? c_debugValue : c_normalValue);
     }
 
     @Override
     public void log(LogRecord record) {
+        if (!this.isLoggable(record.getLevel()))
+            return;
+
         record.setMessage(m_prefix + record.getMessage());
         super.log(record);
     }
