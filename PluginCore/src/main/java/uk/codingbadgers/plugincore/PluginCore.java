8c086e3705550eaa53813437828a8011cc614a4e
James Fitzpatrick <thefishlive@hotmail.com>
2019-07-22 14:57:47 +0100

    Add Vault hooks to plugin core
    This adds vault as a hard dependency to PluginCore, in production this will always be true however it will need to be added to any dev environments.

--- PluginCore/src/main/java/uk/codingbadgers/plugincore/PluginCore.java
+++ PluginCore/src/main/java/uk/codingbadgers/plugincore/PluginCore.java
@@ -1,5 +1,10 @@
 package uk.codingbadgers.plugincore;
 
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.ServicesManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import uk.codingbadgers.plugincore.commands.CommandManager;
 import uk.codingbadgers.plugincore.modules.ModuleLoader;
@@ -13,6 +18,10 @@ public class PluginCore extends JavaPlugin {
     private final MessageSystem m_messageSystem;
     private ModuleLoader m_moduleLoader;
     private ModuleCommandSystem m_commandSystem;
+
+    private Economy m_economy = null;
+    private Permission m_permissions = null;
+    private Chat m_chat = null;
 
     public PluginCore() {
         m_messageSystem = new MessageSystem(this);
@@ -28,6 +37,8 @@ public class PluginCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        setupVault();
+
         getCommand("PluginCore").setExecutor(new CommandManager(this, m_messageSystem));
 
         m_moduleLoader.enable();
@@ -43,11 +54,41 @@ public class PluginCore extends JavaPlugin {
         getLogger().log(Level.INFO, "Disabled " + getDescription().getName());
     }
 
+    private void setupVault() {
+        ServicesManager servicesManager = getServer().getServicesManager();
+
+        RegisteredServiceProvider<Economy> eco_rsp = servicesManager.getRegistration(Economy.class);
+        if (eco_rsp != null) {
+            m_economy = eco_rsp.getProvider();
+        }
+
+        RegisteredServiceProvider<Permission> permissions_rsp = servicesManager.getRegistration(Permission.class);
+        if (permissions_rsp != null) {
+            m_permissions = permissions_rsp.getProvider();
+        }
+
+        RegisteredServiceProvider<Chat> chat_rsp = servicesManager.getRegistration(Chat.class);
+        if (chat_rsp != null) {
+            m_chat = chat_rsp.getProvider();
+        }
+    }
+
     public ModuleLoader getModuleLoader() {
         return m_moduleLoader;
     }
 
     public ModuleCommandSystem getCommandSystem() {
         return m_commandSystem;
+    }
+    public Economy getVaultEconomy() {
+        return m_economy;
+    }
+
+    public Permission getVaultPermissions() {
+        return m_permissions;
+    }
+
+    public Chat getVaultChat() {
+        return m_chat;
     }
 }
